1c3e1726eaa1825e041519d6e0c628540c99a742
alumno <alumno@E521>
2019-09-26 12:42:07 -0700

    FInal Usuario

--- ControlesDeUsuario/MainWindow.xaml.cs
+++ ControlesDeUsuario/MainWindow.xaml.cs
@@ -54,5 +54,102 @@ namespace ControlesDeUsuario
                     break;
             }
         }
+
+        private void BtnCalcularArea_Click(object sender, RoutedEventArgs e)
+        {
+            double area = 0.0;
+
+            switch (cbFigura.SelectedIndex)
+            {
+                case 0: //circulo
+                    double radio =
+                        double.Parse(
+                    ((ParametrosCirculo)
+                        (gridParametrosFigura.Children[0])).txtRadio.Text);
+
+                    area = Math.PI * radio * radio;
+                    break;
+
+                case 1: //Triangulo
+                    double AlturaT =
+                        double.Parse(
+                    ((ParametrosTriangulo)
+                        (gridParametrosFigura.Children[0])).txtAlturaTriangulo.Text);
+                    double BaseT =
+                        double.Parse(
+                    ((ParametrosTriangulo)
+                        (gridParametrosFigura.Children[0])).txtBaseTriangulo.Text);
+
+                    area = (AlturaT * BaseT) / 2;
+                    break;
+
+                case 2: //Rectangulo
+                    double AlturaR =
+                        double.Parse(
+                    ((ParametrosRectangulo)
+                        (gridParametrosFigura.Children[0])).txtAlturaRectangulo.Text);
+                    double BaseR =
+                        double.Parse(
+                    ((ParametrosRectangulo)
+                        (gridParametrosFigura.Children[0])).txtBaseRectangulo.Text);
+
+                    area = AlturaR * BaseR;
+                    break;
+
+                case 3: //Cuadrado
+                    double AlturaC =
+                        double.Parse(
+                    ((ParametrosCuadrado)
+                        (gridParametrosFigura.Children[0])).txtAlturaC.Text);
+                    double BaseC =
+                    double.Parse(
+                ((ParametrosCuadrado)
+                    (gridParametrosFigura.Children[0])).txtBaseC.Text);
+
+                    area = AlturaC * BaseC;
+                    break;
+
+               case 4: //Pentagono
+                    double Perimetro =
+                        double.Parse(
+                    ((ParametrosPentagono)
+                        (gridParametrosFigura.Children[0])).txtPerimetro.Text);
+                    double Apotema =
+                      double.Parse(
+                  ((ParametrosPentagono)
+                      (gridParametrosFigura.Children[0])).txtApotema.Text);
+
+                    area = (Perimetro * Apotema) / 2;
+
+                    break;
+
+                    
+                 case 5: //Trapecio
+                    double BaseMen =
+                        double.Parse(
+                    ((ParametrosTrapecio)
+                        (gridParametrosFigura.Children[0])).txtBaseMenor.Text);
+                    double BaseMay =
+                       double.Parse(
+                   ((ParametrosTrapecio)
+                       (gridParametrosFigura.Children[0])).txtBaseMayor.Text);
+                    double AlturaTR =
+                       double.Parse(
+                   ((ParametrosTrapecio)
+                       (gridParametrosFigura.Children[0])).txtAlturaTrapecio.Text);
+
+                    area= ((BaseMay+BaseMen)*AlturaTR) / 2;
+
+                    break;
+
+
+
+
+
+                default:
+                    break;
+            }
+            txtAreaGeneral.Text = area.ToString();
+        }
     }
 }
