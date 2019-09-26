98632bdc4b78b02ae05c7c79519eb8699107667d
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-26 07:46:44 -0700

    Agregar archivos de proyecto.

---
+++ ControlesDeUsuario/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlesDeUsuario
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CbFigura_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            gridParametrosFigura.Children.Clear();
+            switch(cbFigura.SelectedIndex)
+            {
+                case 0: //Circulo
+                    gridParametrosFigura.Children.Add(new ParametrosCirculo());
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
